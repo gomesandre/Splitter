fd350ba21a163a71b81c52e8c8358e0c368844d1
André Gomes <andregms@outlook.com>
2019-11-20 14:45:30 -0300

    [ADD] Split function + Tests to cover split function

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -8,19 +8,19 @@ contract Splitter {
   }
 
   mapping (address => Member) public members;
-  uint public membersCount;
+  address[] public addressIndices;
 
   function totalMembers() public view returns (uint) {
-      return membersCount;
+      return addressIndices.length;
   }
 
   function enter(string memory _name) public {
     members[msg.sender] = Member(msg.sender, _name, true);
-    membersCount++;
+    addressIndices.push(msg.sender);
   }
 
   modifier onlyMember() {
-    require(isMember(), "This method is restricted to contract members.");
+    require(isMember(), "This method is restricted to contract members!");
     _;
   }
 
@@ -28,7 +28,16 @@ contract Splitter {
     return members[msg.sender].exists;
   }
 
-  function split() public view onlyMember returns (string memory) {
-    return "Account registered as member of contract!";
+  function split() public payable onlyMember {
+    require(msg.value >= 0.1 ether, "Send at least 0.1 ether to split!");
+
+    uint length = addressIndices.length - 1;
+    uint splitted = msg.value / length;
+
+    for(uint i = 0; i < addressIndices.length; i++) {
+      if(members[addressIndices[i]].account != msg.sender) {
+        members[addressIndices[i]].account.transfer(splitted);
+      }
+    }
   }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -12,6 +12,11 @@ contract("Splitter", function(_accounts) {
     return await SplitterInstance.members(account);
   };
 
+  const getBalance = async (account) => {
+    let balance = await web3.eth.getBalance(account);
+    return web3.utils.fromWei(balance);
+  };
+
   beforeEach( async () => {
     SplitterInstance = await Splitter.new({ from: _accounts[0] });
   })
@@ -42,20 +47,45 @@ contract("Splitter", function(_accounts) {
   })
 
   describe('Check if isMember modifier is working', function() {
-    it('should call function split', async () => {
-      addMember("Delta", _accounts[1]);
-      const splitResponse = await SplitterInstance.split({ from: _accounts[1] });
-      assert.equal(splitResponse, "Account registered as member of contract!");
-    })
-
-    it('should not call function split', async () => {
+    it('should not call function split because user is not a member', async () => {
       try {
         await SplitterInstance.split({ from: _accounts[0] });
       } catch (err) {
-        assert.equal(err.message, "Returned error: VM Exception while processing transaction: revert This method is restricted to contract members.");
+        assert.equal(err.message, "Returned error: VM Exception while processing transaction: revert This method is restricted to contract members! -- Reason given: This method is restricted to contract members!.");
+      }
+    })
+  })
+    
+  describe('Check if split function is working well', function() {
+    it('should call function split and return minimum value error', async () => {
+      addMember("Delta", _accounts[1]);
+      
+      try {
+        await SplitterInstance.split({ from: _accounts[1] });
+      } catch (err) {
+        assert.equal(err.message, "Returned error: VM Exception while processing transaction: revert Send at least 0.1 ether to split! -- Reason given: Send at least 0.1 ether to split!.")
       }
     })
 
-  }) 
+    it('should call function and split ether', async () => {
+      addMember("Foxtrot", _accounts[3]);
+      addMember("Golf", _accounts[4]);
+      addMember("Hotel", _accounts[5]);
+      
+      const foxtrot = await getBalance(_accounts[3]);
+      const golf = await getBalance(_accounts[4]);
+      const hotel = await getBalance(_accounts[5]);
+      
+      try {
+        await SplitterInstance.split({ from: _accounts[5], value: 2000000000000000000 });
+      } catch (err) {
+        assert(err);
+      }
+
+      assert(foxtrot < await getBalance(_accounts[3]));
+      assert(golf < await getBalance(_accounts[4]));
+      assert(hotel > await getBalance(_accounts[5]));
+    })    
+  })
 
 });
